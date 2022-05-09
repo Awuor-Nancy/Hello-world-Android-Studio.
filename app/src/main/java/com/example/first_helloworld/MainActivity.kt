eb261ed685ec82fdb09838c178dfd7edda3d6db9
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-05-09 10:03:19 +0300

    commit first_app

---
+++ app/src/main/java/com/example/first_helloworld/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.first_helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBmi:Button
+    lateinit var btnSend:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi = findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+            val intent = Intent(this,BMICalculate::class.java)
+            startActivity(intent)
+        }
+        btnSend = findViewById (R.id.btnSend)
+        btnSend.setOnClickListener {
+            val intent = Intent(this,sendCash::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/first_helloworld/sendCash.kt
@@ -0,0 +1,11 @@
+package com.example.first_helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sendCash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_cash)
+    }
+}
